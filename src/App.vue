61336919bc258370892f03345ad479882fb81ff3
Zhalgas Azhibayev <g.zhiga@gmail.com>
2022-12-30 12:19:49 +0600

    vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,40 @@
 <template lang="pug">
-  router-view
+  v-app
+    v-app-bar(app color='primary' dark)
+      .d-flex.align-center
+        v-img.shrink.mr-2(
+          alt='Vuetify Logo'
+          contain
+          src='https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png'
+          transition='scale-transition'
+          width='40'
+        )
+        v-img.shrink.mt-1.hidden-sm-and-down(
+          alt='Vuetify Name'
+          contain
+          min-width='100'
+          src='https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png'
+          width='100'
+        )
+      v-spacer
+      v-btn(
+        href='https://github.com/vuetifyjs/vuetify/releases/latest'
+        target='_blank'
+        text
+      )
+        v-icon mdi-open-in-new
+        span.mr-2 Latest Release
+    v-main
+      router-view
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
