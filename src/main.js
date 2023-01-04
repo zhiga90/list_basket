7168756a22973b184d2149fa3649138d975e5757
Zhalgas Azhibayev <g.zhiga@gmail.com>
2023-01-04 16:44:01 +0600

    axios

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import dummyJSON from './plugins/axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$api = dummyJSON
 
 new Vue({
   router,
